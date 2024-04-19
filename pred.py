75b7ce0784d5458da4f896d3178b61cde9a98e02
uranc <cemuran@gmail.com>
2024-04-19 15:22:58 +0200

    f1 test

--- pred.py
+++ pred.py
@@ -52,10 +52,7 @@ if mode == 'train':
     
     # [traces, labels] = next(iter(train_dataset))
     # # for traces, labels in next(iter(train_dataset)):
-    # print(traces.shape)
-    # print(labels.shape)
-    # print(labels)
-    # pdb.set_trace()
+    
     # train 
     from model.training import train_pred
     
@@ -91,19 +88,9 @@ elif mode == 'predict':
     timesteps = params['NO_TIMEPOINTS']
     for LFP in val_datasets:
         LFP=LFP[:len(LFP)-len(LFP)%timesteps,:].reshape(-1,timesteps,n_channels)
-        # y_predict = np.zeros(shape=(LFP.shape[0]))
         windowed_signal = model.predict(LFP,verbose=1)
-        # pdb.set_trace()
         probs = np.hstack(windowed_signal)
-        # y_predict = probs.mean(axis=1)
         val_pred.append(probs)
-        # for i,window in enumerate(windowed_signal):
-        #     print(window.shape)
-            # if len(window) != timesteps:
-            #     pdb.set_trace()
-            #     y_predict[i*timesteps:i*timesteps+len(window)]=window
-            # else:
-            #     y_predict[i*timesteps:(i+1)*timesteps]=window
 
     # Validation plot in the second ax
     all_pred_events = []
@@ -115,11 +102,9 @@ elif mode == 'predict':
             _,_,F1_val[j,i],_,_,_=get_performance(pred_val_events,val_labels[j],verbose=False)
             tmp_pred.append(pred_val_events)
         all_pred_events.append(tmp_pred)
-    # pdb.set_trace()
     
-    
-    best_preds = all_pred_events[0][14]
-    pdb.set_trace()
+    # pick model
+    best_preds = all_pred_events[0][15]
     pred_vec = np.zeros(val_datasets[0].shape[0])
     label_vec = np.zeros(val_datasets[0].shape[0])
         
@@ -132,47 +117,9 @@ elif mode == 'predict':
     import matplotlib.pyplot as plt
     for pred in best_preds:
         rip_begin = int(pred[0]*1250)
-        # ss = np.where(np.abs(val_labels[0][:,0]-pred[0])==np.min(np.abs(val_labels[0][:,0]-pred[0])))[0]
-        # label_begin = val_labels[0][int(ss*1250), 0]
-        
         plt.plot(val_datasets[0][rip_begin-128:rip_begin+128, :])
         plt.plot(pred_vec[rip_begin-128:rip_begin+128], 'r')
         plt.plot(label_vec[rip_begin-128:rip_begin+128], 'k')
         plt.show()
-    
-    # F1_train=np.empty(shape=len(th_arr))
-    # F1_test=np.empty(shape=len(th_arr))
-    # for i,th in enumerate(th_arr):
-    #     pdb.set_trace()
-    #     pred_train_events=get_predictions_index(y_pred_train,th)/1250
-    #     pred_test_events=get_predictions_index(y_pred_test,th)/1250
-    #     _,_,F1_train[i],_,_,_=get_performance(pred_train_events,events_train,verbose=False)
-    #     _,_,F1_test[i],_,_,_=get_performance(pred_test_events,events_test,verbose=False)
-    
-
-    # fig,axs=plt.subplots(1,2,figsize=(12,5),sharey='all')
-    # axs[0].plot(th_arr,F1_train,'k.-')
-    # axs[0].plot(th_arr,F1_test,'b.-')
-    # axs[0].legend(['Train','Test'])
-    # axs[0].set_ylim([0 ,max(max(F1_train), max(F1_test)) + 0.1])
-    # axs[0].set_title('F1 test and train')
-    # axs[0].set_ylabel('F1')
-    # axs[0].set_xlabel('Threshold')
-
-
-    # # Validation plot in the second ax
-    # F1_val=np.zeros(shape=(len(LFP_val),len(th_arr)))
-    # for j,pred in enumerate(val_pred):
-    #     for i,th in enumerate(th_arr):
-    #         pred_val_events=get_predictions_index(pred,th)/1250
-    #         _,_,F1_val[j,i],_,_,_=get_performance(pred_val_events,GT_val[j],verbose=False)
-
-    # for i in range(len(LFP_val)):
-    #     axs[1].plot(th_arr,F1_val[i])
-    # axs[1].plot(th_arr,np.mean(F1_val,axis=0),'k.-')
-    # axs[1].set_title('Validation F1')
-    # axs[1].set_ylabel('F1')
-    # axs[1].set_xlabel('Threshold')
-       
     
     
