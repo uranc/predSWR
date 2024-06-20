c23f46f55a2ccb0ae41bbeb2ec46e0a37d182135
uranc <cemuran@gmail.com>
2024-06-20 10:03:25 +0200

    clean up comments

--- pred.py
+++ pred.py
@@ -84,14 +84,6 @@ if mode == 'train':
     train_size = len(list(train_dataset))
 
     params['RIPPLE_RATIO'] = label_ratio
-    # model
-    # from model.model_fn import build_Prida_LSTM
-    # model = build_Prida_LSTM([params["NO_TIMEPOINTS"],params["NO_CHANNELS"]])
-
-
-    # [traces, labels] = next(iter(train_dataset))
-    # pdb.set_trace()
-    # # for traces, labels in next(iter(train_dataset)):
 
     # train
     from model.training import train_pred
@@ -191,28 +183,14 @@ elif mode == 'predict':
             model = build_DBI_TCN(params["NO_TIMEPOINTS"], params=params)
     model.summary()
 
-    # from model.model_fn import build_DBI_TCN
-    # pdb.set_trace()
-    # model = build_DBI_TCN(params["NO_TIMEPOINTS"], params=params)
-    # model.summary()
     params['BATCH_SIZE'] = 512*2
-
-    # pdb.set_trace()
-    # noise = np.random.rand(1024,8).reshape(int(1024/32),32,8)
-    # probs = model.predict(noise)
-
-    # get inputs
-    # a_input = importlib.import_module('experiments.{0}.model.input_fn'.format(model_name))
-    # rippleAI_load_dataset = getattr(a_input, 'rippleAI_load_dataset')
     from model.input_fn import rippleAI_load_dataset
 
     preproc = False if model_name=='RippleNet' else True
     val_datasets, val_labels = rippleAI_load_dataset(params, mode='test', preprocess=preproc)
 
-    # for j, labels in enumerate(val_labels):
-    #     np.save('/mnt/hpc/projects/OWVinckSWR/DL/predSWR/labels_val{0}.npy'.format(j), labels)
-    # pdb.set_trace()
     from model.cnn_ripple_utils import get_predictions_index, get_performance
+
     # get predictions
     val_pred = []
     th_arr=np.linspace(0.1,0.9,19)
@@ -224,19 +202,9 @@ elif mode == 'predict':
     # get predictions
     sample_length = params['NO_TIMEPOINTS']*2
 
-    # val_datasets[0] = val_datasets[0][89500:90500-500,:]
-    # val_datasets[1] = val_datasets[1][:100,:]
-    val_datasets = [val_datasets[3]]
-    val_labels = [val_labels[3]]
-    # test_end = val_datasets[0].shape[0]
-    # val_batches = []
-    # for i in range(0, test_end - timesteps, 1):
-    #     val_batches.append(val_datasets[0][np.arange(i, i + timesteps), :])
-    # val_batches = np.array(val_batches)
+    # val_datasets = [val_datasets[3]]
+    # val_labels = [val_labels[3]]
     for LFP in val_datasets:
-        # test_end = LFP.shape[0]
-        # LFP=LFP[:len(LFP)-len(LFP)%timesteps,:].reshape(-1,timesteps,n_channels)
-        # aa = []
         if model_name == 'RippleNet':
             train_x = timeseries_dataset_from_array(LFP[:,4]/1000, None, sequence_length=sample_length, sequence_stride=1, batch_size=params["BATCH_SIZE"])
             windowed_signal = np.squeeze(model.predict(train_x, verbose=1))
@@ -245,13 +213,8 @@ elif mode == 'predict':
             train_x = timeseries_dataset_from_array(LFP, None, sequence_length=sample_length, sequence_stride=1, batch_size=params["BATCH_SIZE"])
             windowed_signal = np.squeeze(model.predict(train_x, verbose=1))
             probs = np.hstack((np.zeros((50, 1)).flatten(),windowed_signal[0,:-1], windowed_signal[:, -1]))
-
-        # probs = np.hstack(aa)
-        # pdb.set_trace()
-        # probs = np.hstack(windowed_signal)
         val_pred.append(probs)
 
-    # pdb.set_trace()
     # Validation plot in the second ax
     all_pred_events = []
     precision=np.zeros(shape=(len(val_datasets),len(th_arr)))
@@ -265,21 +228,17 @@ elif mode == 'predict':
         # tmp_IOUs = []
         for i,th in enumerate(th_arr):
             pred_val_events = get_predictions_index(pred,th)/1250
-            # pdb.set_trace()
             [precision[j,i], recall[j,i], F1_val[j,i], tmpTP, tmpFN, tmpIOU] = get_performance(pred_val_events,val_labels[j],verbose=False)
             TP[j,i] = tmpTP.sum()
             FN[j,i] = tmpFN.sum()
             IOU[j,i] = np.median(tmpIOU.sum(axis=0))
             tmp_pred.append(pred_val_events)
-        # pdb.set_trace()
-        # IOU.append(np.array(tmp_IOUs))
         all_pred_events.append(tmp_pred)
     pdb.set_trace()
+
     # pick model
     print(F1_val[0])
-    # pdb.set_trace()
     mind = np.argmax(F1_val[0])
-    # print(all_pred_events[0][mind])
     best_preds = all_pred_events[0][mind]
     pred_vec = np.zeros(val_datasets[0].shape[0])
     label_vec = np.zeros(val_datasets[0].shape[0])
@@ -289,13 +248,7 @@ elif mode == 'predict':
 
     for lab in val_labels[0]:
         label_vec[int(lab[0]*1250):int(lab[1]*1250)] = 0.9
-
-
-    # mean_median_IOU = np.stack((np.stack((np.nanmean(IOU[0], axis=1), np.nanmedian(IOU[0],axis=1)), axis=-1),
-    #                             np.stack((np.nanmean(IOU[1], axis=1), np.nanmedian(IOU[1],axis=1)), axis=-1)),axis=0)
-    # mean_median_IOU[:,:,0], mean_median_IOU[:,:,1]
     stats = np.stack((precision, recall, F1_val, TP, FN, IOU), axis=-1)
-    # pdb.set_trace()
 
     for j,pred in enumerate(val_pred):
         np.save('/mnt/hpc/projects/OWVinckSWR/DL/predSWR/probs/preds_val{0}_{1}.npy'.format(j, model_name), pred)
@@ -396,7 +349,6 @@ elif mode == 'predictSynth':
             # get model
             a_model = importlib.import_module('experiments.{0}.model.model_fn'.format(model))
             build_DBI_TCN = getattr(a_model, 'build_DBI_TCN')
-            # from model.model_fn import build_DBI_TCN
 
             params['WEIGHT_FILE'] = 'experiments/{0}/'.format(model_name)+'weights.last.h5'
             model = build_DBI_TCN(params["NO_TIMEPOINTS"], params=params)
@@ -409,12 +361,6 @@ elif mode == 'predictSynth':
         synth /= 2
 
     synth = synth[9*1250:-1250*12]
-    # train_end = synth.shape[0]
-    # timesteps = 50
-    # synth_batches = []
-    # for i in range(0, train_end - timesteps, 1):
-    #     synth_batches.append(synth[np.arange(i, i + timesteps), :])
-    # synth_batches = np.array(synth_batches)
     from keras.utils import timeseries_dataset_from_array
 
     # get predictions
@@ -423,31 +369,21 @@ elif mode == 'predictSynth':
     timesteps = params['NO_TIMEPOINTS']
     sample_length = params['NO_TIMEPOINTS']*2
     train_x = timeseries_dataset_from_array(synth, None, sequence_length=sample_length, sequence_stride=1, batch_size=params["BATCH_SIZE"])
-    # synth=synth[:len(synth)-len(synth)%timesteps,:].reshape(-1,timesteps,n_channels)
-    # probs = np.squeeze(model.predict(train_x))
     probs = np.squeeze(model.predict(train_x))
-    # pdb.set_trace()
     probs = np.hstack((probs[0,:-1], probs[:, -1]))
 
     def moving_average(x, w):
         return np.convolve(x, np.ones(w), 'valid') / w
     # probs = moving_average(probs, 4)
 
-    # pdb.set_trace()
-    # synth=np.expand_dims(synth, axis=0)
-    # pdb.set_trace()
     from scipy.signal import decimate
     import matplotlib.pyplot as plt
     synth = np.load('/mnt/hpc/projects/OWVinckSWR/DL/predSWR/synth_stim.npy')
     synth = (synth-np.min(synth))/(np.max(synth)-np.min(synth))
     synth = synth[50+9*1250:-1250*12,:]
-    # pdb.set_trace()
     tt = np.arange(synth.shape[0])/1250
-    # pdb.set_trace()
-    # plt.plot(decimate(tt, 2), decimate(synth[:, 0], 2))
     plt.plot(tt, synth[:, 0])
     tt = np.arange(probs.shape[0])/1250
-    # plt.plot(decimate(tt,4), decimate(probs, 4))
     plt.plot(tt, probs)
     plt.show()
     pdb.set_trace()
@@ -464,13 +400,13 @@ elif mode == 'predictPlot':
     # get inputs
     a_input = importlib.import_module('experiments.{0}.model.input_fn'.format(model))
     rippleAI_load_dataset = getattr(a_input, 'rippleAI_load_dataset')
+    
     # from model.input_fn import rippleAI_load_dataset
     val_datasets, val_labels = rippleAI_load_dataset(params, mode='test')
 
     # probs
     val_id = 0
     val_pred = np.load('/mnt/hpc/projects/OWVinckSWR/DL/predSWR/probs/preds_val{0}_{1}.npy'.format(val_id, model_name))
-    # val_pred = np.hstack((np.zeros((50,1)).flatten(), val_pred))
     val_pred = [val_pred]
 
     val_datasets = [val_datasets[val_id]]
@@ -484,18 +420,13 @@ elif mode == 'predictPlot':
         for i,th in enumerate(th_arr):
             pred_val_events=get_predictions_index(pred,th)/1250
             _,_,F1_val[j,i],_,_,_=get_performance(pred_val_events ,val_labels[j], verbose=False)
-            # if i==12:
-            #     [precision, recall, F1, TP, FN, IOU]=get_performance(pred_val_events ,val_labels[j], exclude_matched_trues=True, verbose=False)
-            #     pdb.set_trace()
             tmp_pred.append(pred_val_events)
         all_pred_events.append(tmp_pred)
 
     # pick model
     print(F1_val[0])
     print(F1_val[1])
-    # pdb.set_trace()
     mind = np.argmax(F1_val[0])
-    # print(all_pred_events[0][mind])
     best_preds = all_pred_events[0][mind]
     pred_vec = np.zeros(val_datasets[0].shape[0])
     label_vec = np.zeros(val_datasets[0].shape[0])
@@ -505,7 +436,6 @@ elif mode == 'predictPlot':
 
     for lab in val_labels[0]:
         label_vec[int(lab[0]*1250):int(lab[1]*1250)] = 0.9
-    # pdb.set_trace()
     for pred in val_labels[0]:
         rip_begin = int(pred[0]*1250)
         plt.plot(val_datasets[0][rip_begin-256:rip_begin+256, :]/3, 'gray')
@@ -513,4 +443,3 @@ elif mode == 'predictPlot':
         plt.plot(val_pred[0][rip_begin-256:rip_begin+256]*pred_vec[rip_begin-256:rip_begin+256], 'r')
         plt.plot(label_vec[rip_begin-256:rip_begin+256], 'k')
         plt.show()
-        # pdb.set_trace()
